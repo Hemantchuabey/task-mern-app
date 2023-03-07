8089b844eec0ae3f43241c291f99d24361e2d4a1
Hemant <hementchaubey@gmail.com>
2023-03-07 09:03:51 +0530

    Added all the routes for CRUD application

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use("/api/goals", require("./routes/taskRoutes"));
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
