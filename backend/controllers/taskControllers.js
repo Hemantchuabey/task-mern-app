b9d791e00f5cd9db6fb9a7279ceca5b5c24db1d6
Hemant <hementchaubey@gmail.com>
2023-03-10 21:04:07 +0530

    to use error handler instead of try catch we use express-async-handler to bind each functionality.

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc : GET tasks
 // @route : GET /api/goals
 // @access : Private after auth
-const getTask = (req, res) => {
-  res.status(200).json({ message: "get goals" });
-};
+const getTask = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: "get goals" });
+});
 
 // @desc : SET tasks
 // @route : SET /api/goals
 // @access : Private after auth
-const setTask = (req, res) => {
+const setTask = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please include Text field!!");
   }
   // console.log(req.body);
   return res.status(200).json({ message: "set goals" });
-};
+});
 
 // @desc : UPDATE tasks
 // @route : UPDATE /api/goals/:id
 // @access : Private after auth
-const updateTask = (req, res) => {
-  res.status(200).json({ message: `update goals ${req.params.id}` });
-};
+const updateTask = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: `update goals ${req.params.id}` });
+});
 
 // @desc : DELETE tasks
 // @route : DELETE /api/goals/:id
 // @access : Private after auth
-const deleteTask = (req, res) => {
-  res.status(200).json({ message: `delete goals ${req.params.id}` });
-};
+const deleteTask = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: `delete goals ${req.params.id}` });
+});
 
 module.exports = {
   getTask,
