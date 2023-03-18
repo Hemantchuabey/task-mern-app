d3a56cc9aa2a7a312d8e5de26948f6a83a25428b
Hemant <hementchaubey@gmail.com>
2023-03-18 18:35:17 +0530

    Added Login page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -74,7 +74,7 @@ const Register = () => {
           </div>
           <div className="form-group">
             <button type="submit" className="btn btn-block">
-              Submit
+              Sign In
             </button>
           </div>
         </form>
